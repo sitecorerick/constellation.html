fc743aebfc4b55cdba3d35ed8e7f9a3e84c2cd32
Richard Cabral <sitecorerick@gmail.com>
2013-11-26 14:47:48 -0500

    Added H5 and H6 tags

---
+++ RenderH5Extension.cs
@@ -0,0 +1,36 @@
+﻿namespace Spark.Html
+{
+	using System.Diagnostics.CodeAnalysis;
+	using System.Web.UI;
+
+	/// <summary>
+	/// Extension to make working with HtmlTextWriter easier.
+	/// </summary>
+	[SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", Justification = "HTML Tags are not English, FX Cop. Stop correcting my spelling.")]
+	public static class RenderH5Extension
+	{
+		/// <summary>
+		/// Creates a H4 HTML tag and adds any provided attributes to the tag.
+		/// </summary>
+		/// <param name="writer">The HTMLTextWriter.</param>
+		/// <param name="id">The value of the id attribute.</param>
+		/// <param name="cssClass">The value of the class attribute.</param>
+		/// <returns>H4 HTML tag with attributes.</returns>
+		[SuppressMessage("Microsoft.Design", "CA1026:DefaultParametersShouldNotBeUsed", Justification = "Welcome to .NET 4.0 FX Cop.")]
+		public static HtmlTag RenderH5(this HtmlTextWriter writer, string id = null, string cssClass = null)
+		{
+			return RenderTagExtension.RenderTag(writer, HtmlTextWriterTag.H5, id, cssClass);
+		}
+
+		/// <summary>
+		/// Creates a H4 HTML tag and adds any provided attributes to the tag.
+		/// </summary>
+		/// <param name="writer">The HTMLTextWriter.</param>
+		/// <param name="attributes">HTML attributes.</param>
+		/// <returns>H4 HTML tag with attributes.</returns>
+		public static HtmlTag RenderH5(this HtmlTextWriter writer, params HtmlAttribute[] attributes)
+		{
+			return new HtmlTag(writer, HtmlTextWriterTag.H4, attributes);
+		}
+	}
+}
